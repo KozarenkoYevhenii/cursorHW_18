af7d2f8cd81f6575329ec313d289cbbc58f2c3dd
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-25 16:20:17 +0300

    Updates

---
+++ precache-manifest.bdf2b1069c7cf235c967720ebd819287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ec26ab32d131cd93753ad6fda33f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d5c7140319a1f7caa6",
+    "url": "/static/css/main.cefb7abd.chunk.css"
+  },
+  {
+    "revision": "fd68e51bbf5ddd5a9feb",
+    "url": "/static/js/2.df5f2c64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df5f2c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d5c7140319a1f7caa6",
+    "url": "/static/js/main.55645d29.chunk.js"
+  },
+  {
+    "revision": "f90a2374f3bf27c98273",
+    "url": "/static/js/runtime-main.8e264a9d.js"
+  }
+]);
